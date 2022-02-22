b1b7ead12b46e1c01c1dafead30eaa5d150853d4
jHaben <jrhaben@gmail.com>
2022-02-21 20:03:17 -0600

    add daytimeHalfmVm zone

--- js/parse.js
+++ js/parse.js
@@ -1,5 +1,5 @@
-export function textToRings(rings) {
-    const rings = rings.split('\n').map((line) => {
+export function textToRings(text) {
+    const rings = text.split('\n').map((line) => {
         let coords = line.substring(line.indexOf('> ') + 2)
         coords = coords.slice(0, -2)
         return coords.split(',').map((it) => Number(it))
